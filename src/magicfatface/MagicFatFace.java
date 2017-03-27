0d2336e2a95f7815760b6edb86b1b5f875cde4a8
TimonVerbeke <timon.verbeke@student.howest.be>
2017-03-27 11:52:28 +0200

    Initial commit

--- src/magicfatface/MagicFatFace.java
+++ src/magicfatface/MagicFatFace.java
@@ -11,11 +11,6 @@ package magicfatface;
  */
 public class MagicFatFace {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    String Timon = "Cyka Blyat";
     
 }
